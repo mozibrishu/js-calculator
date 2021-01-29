2dd332312ee2e22e46a1268bb78de44a409082bd
mozibrishu <mozib.diu@gmail.com>
2021-01-29 03:37:33 -0600

    Operator Handling Running

--- js/script.js
+++ js/script.js
@@ -1,15 +1,72 @@
+let firstValue = null;
+let secondValue = null;
+let result = null;
+let mainOperator = null;
+
+function digitHandler(digit){
+    let currentScreen = getScreen();
+    if(isOnlyDigitZero(currentScreen)){
+        setScreen(digit);
+    }
+    else{
+        setScreen(currentScreen + digit);
+    }    
+}
+
+function isOnlyDigitZero(currentScreen){
+    if(currentScreen[0] == '0' && currentScreen.length == 1){
+        return true;
+    }
+    else{
+        return false
+    }
+}
+
+function operatorHandler(operator){
+    firstValue = parseFloat(getScreen());
+    setScreen(operator);
+    mainOperator = operator;
+}
+
+function equalHandler(){
+    secondValue = parseFloat(getScreen());
+    result = calculateValue();
+    setScreen(result);
+}
+
+function calculateValue(){
+    if(mainOperator != null && firstValue !=null && secondValue !=null){
+        switch(mainOperator){
+            case '+':
+                return firstValue + secondValue;
+            case '-':
+                return firstValue - secondValue;
+        }
+    }
+}
+
+function dotHandler(dot){
+    let currentScreen = getScreen();
+    if(!(isAlreadyDot(currentScreen))){
+        setScreen(currentScreen + dot);
+    }
+}
 
 
-
-
-
-
+function isAlreadyDot(currentScreen){
+    for(let i = 0; i<currentScreen.length; i++){
+        if(currentScreen[i] == '.'){
+            return true;
+        }
+    }
+    return false;
+}
 
 
 // Get Set Method
-function getAmount(id) {
-    return parseFloat(document.getElementById(id).innerText);
+function getScreen() {
+    return document.getElementById("display").innerText;
 }
-function setAmount(id, amount) {
-    document.getElementById(id).innerText = amount;
+function setScreen(value) {
+    document.getElementById("display").innerText = value;
 }
